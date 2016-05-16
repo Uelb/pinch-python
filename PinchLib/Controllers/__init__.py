a3c42841e82b8c3a2debf6099f109f29355a585e
Thomas THIMOTHEE <thomas@oxynum.fr>
2016-05-16 11:06:25 +0200

    configure package

--- PinchLib/Controllers/__init__.py
+++
@@ -1,4 +0,0 @@
-from PinchLib.Controllers.BaseController import *
-from PinchLib.Controllers.WebhookTypeController import *
-from PinchLib.Controllers.WebhookController import *
-from PinchLib.Controllers.TicketController import *
--- PinchLib/Http/__init__.py
+++
@@ -1,5 +0,0 @@
-from PinchLib.Http.HttpMethodEnum import *
-from PinchLib.Http.HttpRequest import *
-from PinchLib.Http.HttpResponse import *
-from PinchLib.Http.HttpClient import *
-from PinchLib.Http.RequestsClient import *
